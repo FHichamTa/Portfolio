9b6596b9c8f1145c0551b0741b31518b95e0c905
Hicham <gitlab.icg@gmail.com>
2025-04-16 09:34:24 +0200

    csp policy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; img-src 'self' blob: data: http://localhost:8000 https://htaafraouti.fr https://supabase.com https://*.supabase.co https://avatars.githubusercontent.com https://lh3.googleusercontent.com https://frontend-assets.supabase.com; font-src 'self' data: https://fonts.googleapis.com https://cdnjs.cloudflare.com https://frontend-assets.supabase.com https://vercel.live; connect-src 'self' https://*.supabase.co wss://*.supabase.co;"
+          }
+        ]
+      }
+    ];
+  },
 };
 
 export default nextConfig;
