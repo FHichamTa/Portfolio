6cad536eb832cbf24826484eb6a84b04579f72d6
Hicham <htaafraouti@gmail.com>
2025-06-29 12:56:06 +0200

    Update of the portfolio

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,14 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "default-src 'self'; img-src 'self' blob: data: http://localhost:8000 https://htaafraouti.fr https://supabase.com https://*.supabase.co https://avatars.githubusercontent.com https://lh3.googleusercontent.com https://frontend-assets.supabase.com; font-src 'self' data: https://fonts.googleapis.com https://cdnjs.cloudflare.com https://frontend-assets.supabase.com https://vercel.live; connect-src 'self' https://*.supabase.co wss://*.supabase.co;"
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval'", 
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com", 
+              "img-src 'self' blob: data:", 
+              "font-src 'self' data: https://fonts.googleapis.com", 
+              "connect-src 'self'", 
+            ].join("; ")
           }
         ]
       }
